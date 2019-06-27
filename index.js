cb9c46169168e12f8761c06eaee6885b281d024f
bojanKalanj <bojan.kalanj@gmail.com>
2019-06-27 23:43:21 +0200

    Basic express routes, views and styling

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, nex) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+app.listen(3000);
