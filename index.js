662e78b20976c4296450da512ca69586fa3bc8a6
bojanKalanj <bojan.kalanj@gmail.com>
2019-06-29 20:23:58 +0200

    Add ejs templating engine

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,18 @@ const express = require('express');
 const router = express.Router();
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  var { products } = adminData;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
