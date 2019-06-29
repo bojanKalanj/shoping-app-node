7470b0a183150f176a0f7d23234b4bd272547e4d
bojanKalanj <bojan.kalanj@gmail.com>
2019-06-29 23:20:54 +0200

    Adds Product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,27 +1,30 @@
-var products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
-    pageTitle: 'Add products',
-    path: '/add-product',
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
   });
 };
 
-exports.postAddProduct = (req, res, nex) => {
-  products.push({ title: req.body.title });
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
